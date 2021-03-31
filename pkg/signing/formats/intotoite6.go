80e1d6492e49ee0ffdb4823f14026d4dd4f26e9f
Fredrik Skogman <fredrik.skogman@solarwinds.com>
2021-03-31 15:26:10 +0200

    added more fields and capture shasum output (#7)
    * added more fields and capture shasum output
    
    * ignore newlines
    
    * cleaned up the code a little
    
    * use sub from result too

--- pkg/signing/formats/intotoite6.go
+++ pkg/signing/formats/intotoite6.go
@@ -2,10 +2,19 @@ package formats
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
+	"time"
 
 	"github.com/in-toto/in-toto-golang/in_toto"
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
+)
+
+const tektonID = "https://tekton.dev/attestations/chains@v1"
+
+const (
+	commitParam = "CHAINS-GIT_COMMIT"
+	urlParam    = "CHAINS-GIT_URL"
 )
 
 type InTotoIte6 struct {
@@ -14,6 +23,11 @@ type InTotoIte6 struct {
 type artifactResult struct {
 	ParamName  string
 	ResultName string
+}
+
+type vcsInfo struct {
+	Commit string
+	URL    string
 }
 
 func (i *InTotoIte6) CreatePayload(obj interface{}) (interface{}, error) {
@@ -32,7 +46,9 @@ func (i *InTotoIte6) CreatePayload(obj interface{}) (interface{}, error) {
 	// Podname -> Builder.Id
 	// Results with name *-DIGEST -> Subject
 	// Step containers -> Materials
-	// Params with name CHAINS-GIT_* -> Materials
+	// Params with name CHAINS-GIT_* -> Materials and recipe.materials
+	// tekton-chains -> Recipe.type
+	// Taskname -> Recipe.entry_point
 	att := in_toto.Provenance{
 		Attestation: in_toto.Attestation{
 			AttestationType: in_toto.ProvenanceTypeV1,
@@ -40,76 +56,27 @@ func (i *InTotoIte6) CreatePayload(obj interface{}) (interface{}, error) {
 		Builder: in_toto.Builder{
 			ID: tr.Status.PodName,
 		},
+		Recipe: in_toto.Recipe{
+			Type:       tektonID,
+			EntryPoint: tr.Spec.TaskRef.Name,
+		},
+	}
+	if tr.Status.CompletionTime != nil {
+		att.Metadata.BuildTimestamp =
+			tr.Status.CompletionTime.Format(time.RFC3339)
 	}
 
-	results := []artifactResult{}
-	// Scan for digests
-	for _, r := range tr.Status.TaskSpec.Results {
-		if strings.HasSuffix(r.Name, "-DIGEST") {
-			// 7 chars in -DIGEST
-			results = append(results, artifactResult{
-				ParamName:  r.Name[:len(r.Name)-7],
-				ResultName: r.Name,
-			})
-		}
-	}
+	results := getResultDigests(tr)
+	att.Subject = getSubjectDigests(tr, results)
 
-	att.Subject = in_toto.ArtifactCollection{}
 	att.Materials = in_toto.ArtifactCollection{}
-	// Resolve *-DIGEST variables
-	for _, ar := range results {
-		// get subject
-		sub := ""
-		for _, p := range tr.Spec.Params {
-			if ar.ParamName == p.Name {
-				if p.Value.Type != v1beta1.ParamTypeString {
-					continue
-				}
-				sub = p.Value.StringVal
-				break
-			}
+	vcsInfo := getVcsInfo(tr)
+	// Store git rev as Materials and Recipe.Material
+	if vcsInfo != nil {
+		att.Materials[vcsInfo.URL] = in_toto.ArtifactDigest{
+			"git_commit": vcsInfo.Commit,
 		}
-		if sub == "" {
-			// Parameter was not specifed for this task run
-			continue
-		}
-		for _, trr := range tr.Status.TaskRunResults {
-			if trr.Name == ar.ResultName {
-				// Value should be on format:
-				// hashalg:hash
-				d := strings.Split(trr.Value, ":")
-				if len(d) != 2 {
-					continue
-				}
-
-				att.Subject[sub] = in_toto.ArtifactDigest{
-					d[0]: d[1],
-				}
-			}
-		}
-	}
-
-	git_hash := ""
-	git_url := ""
-	// Scan for git params to use for materials
-	for _, p := range tr.Spec.Params {
-		if p.Name == "CHAINS-GIT_COMMIT" {
-			git_hash = p.Value.StringVal
-			continue
-		}
-		if p.Name == "CHAINS-GIT_URL" {
-			git_url = p.Value.StringVal
-			// make sure url is PURL (git+https)
-			if !strings.HasPrefix(git_url, "git+") {
-				git_url = "git+" + git_url
-			}
-			continue
-		}
-	}
-	if git_hash != "" && git_url != "" {
-		att.Materials[git_url] = in_toto.ArtifactDigest{
-			"git_commit": git_hash,
-		}
+		att.Recipe.Material = vcsInfo.URL
 	}
 
 	// Add all found step containers as materials
@@ -141,4 +108,133 @@ func (i *InTotoIte6) CreatePayload(obj interface{}) (interface{}, error) {
 
 func (i *InTotoIte6) Type() PayloadType {
 	return PayloadTypeInTotoIte6
+}
+
+// getResultDigests scans over the task.Status.TaskSpec.Results to find any
+// result that has a name that ends with -DIGEST.
+func getResultDigests(tr *v1beta1.TaskRun) []artifactResult {
+	results := []artifactResult{}
+	// Scan for digests
+	for _, r := range tr.Status.TaskSpec.Results {
+		if strings.HasSuffix(r.Name, "-DIGEST") {
+			// 7 chars in -DIGEST
+			results = append(results, artifactResult{
+				ParamName:  r.Name[:len(r.Name)-7],
+				ResultName: r.Name,
+			})
+		}
+	}
+
+	return results
+}
+
+// getVcsInfo scans over the input parameters and loogs for parameters
+// with specified names.
+func getVcsInfo(tr *v1beta1.TaskRun) *vcsInfo {
+	var v vcsInfo
+	// Scan for git params to use for materials
+	for _, p := range tr.Spec.Params {
+		if p.Name == commitParam {
+			v.Commit = p.Value.StringVal
+			continue
+		}
+		if p.Name == urlParam {
+			v.URL = p.Value.StringVal
+			// make sure url is PURL (git+https)
+			if !strings.HasPrefix(v.URL, "git+") {
+				v.URL = "git+" + v.URL
+			}
+			continue
+		}
+	}
+
+	if v.Commit == "" || v.URL == "" {
+		return nil
+	}
+
+	return &v
+}
+
+// getSubjectDigests uses depends on taskResults with names ending with
+// -DIGEST.
+// To be able to find the resource that matches the digest, it relies on a
+// naming schema for an input parameter.
+// If the output result from this task is foo-DIGEST, it expects to find an
+// parameter named 'foo', and resolves this value to use for the subject with
+// for the found digest.
+// If no parameter is found, we search the results of this task, which allows
+// for a task to create a random resource not known prior to execution that
+// gets checksummed and added as the subject.
+// Digests can be on two formats: $alg:$digest (commonly used for container
+// image hashes), or $alg:$digest $path, which is used when a step is
+// calculating a hash of a previous step.
+func getSubjectDigests(tr *v1beta1.TaskRun, results []artifactResult) in_toto.ArtifactCollection {
+	subs := in_toto.ArtifactCollection{}
+	r := regexp.MustCompile(`(\S+)\s+(\S+)`)
+
+	// Resolve *-DIGEST variables
+Results:
+	for _, ar := range results {
+		// Look for subject amongst the params
+		sub := ""
+		for _, p := range tr.Spec.Params {
+			if ar.ParamName == p.Name {
+				if p.Value.Type != v1beta1.ParamTypeString {
+					continue
+				}
+				sub = p.Value.StringVal
+				break
+			}
+		}
+		if sub == "" {
+			// Loog amongst task results
+			for _, trr := range tr.Status.TaskRunResults {
+				if trr.Name == ar.ParamName {
+					sub = trr.Value
+					break
+				}
+			}
+		}
+
+		if sub == "" {
+			// Parameter was not specifed for this task run
+			continue
+		}
+
+		// Loop over the parameters
+		for _, trr := range tr.Status.TaskRunResults {
+			if trr.Name == ar.ResultName {
+				// Value should be on format:
+				// hashalg:hash
+				// hashalg:hash filename
+				mod := strings.TrimRight(trr.Value, "\n")
+				ah := strings.Split(mod, ":")
+				if len(ah) != 2 {
+					continue
+				}
+				alg := ah[0]
+
+				// Is format "hash filename"
+				groups := r.FindStringSubmatch(ah[1])
+				var hash string
+				if len(groups) == 3 {
+					// Verify filename is equal to sbuject
+					// if groups[2] != sub {
+					// 	continue
+					// }
+					hash = groups[1]
+				} else {
+					hash = ah[1]
+				}
+
+				subs[sub] = in_toto.ArtifactDigest{
+					alg: hash,
+				}
+				// Subject found, go after next result
+				continue Results
+			}
+		}
+	}
+
+	return subs
 }
--- pkg/signing/formats/intotoite6_test.go
+++ pkg/signing/formats/intotoite6_test.go
@@ -24,8 +24,16 @@ var testData1 = `
       {
         "name": "CHAINS-GIT_URL",
         "value": "https://git.test.com"
+      },
+      {
+        "name": "filename",
+        "value": "/bin/ls"
       }
     ],
+    "taskRef": {
+      "name": "test-task",
+      "kind": "Task"
+    },
     "serviceAccountName": "default"
   },
   "status": {
@@ -60,12 +68,20 @@ var testData1 = `
       {
         "name": "IMAGE-DIGEST",
         "value": "sha256:hash4"
+      },
+      {
+        "name": "filename-DIGEST",
+        "value": "sha256:hash5   /bin/ls"
       }
     ],
     "taskSpec": {
       "params": [
         {
           "name": "IMAGE",
+          "type": "string"
+        },
+        {
+          "name": "filename",
           "type": "string"
         },
         {
@@ -89,6 +105,10 @@ var testData1 = `
         {
           "name": "IMAGE-DIGEST",
           "description": "Digest of the image just built."
+        },
+        {
+          "name": "filename-DIGEST",
+          "description": "Digest of the file just built."
         }
       ]
     }
@@ -96,12 +116,15 @@ var testData1 = `
 }
 `
 
-var expected = in_toto.Provenance{
+var expected1 = in_toto.Provenance{
 	Attestation: in_toto.Attestation{
 		AttestationType: in_toto.ProvenanceTypeV1,
 		Subject: in_toto.ArtifactCollection{
 			"test.io/test/image": in_toto.ArtifactDigest{
 				"sha256": "hash4",
+			},
+			"/bin/ls": in_toto.ArtifactDigest{
+				"sha256": "hash5",
 			},
 		},
 		Materials: in_toto.ArtifactCollection{
@@ -122,6 +145,89 @@ var expected = in_toto.Provenance{
 	Builder: in_toto.Builder{
 		ID: "test-pod-name",
 	},
+	Recipe: in_toto.Recipe{
+		Type:       tektonID,
+		EntryPoint: "test-task",
+		Material:   "git+https://git.test.com",
+	},
+}
+
+var testData2 = `
+{
+  "spec": {
+    "params": [],
+    "taskRef": {
+      "name": "test-task",
+      "kind": "Task"
+    },
+    "serviceAccountName": "default"
+  },
+  "status": {
+    "conditions": [
+      {
+        "type": "Succeeded",
+        "status": "True",
+        "lastTransitionTime": "2021-03-29T09:50:15Z",
+        "reason": "Succeeded",
+        "message": "All Steps have completed executing"
+      }
+    ],
+    "podName": "test-pod-name",
+    "steps": [
+      {
+        "name": "step1",
+        "container": "step-step1",
+        "imageID": "docker-pullable://gcr.io/test1/test1@sha256:hash1"
+      }
+    ],
+    "taskResults": [
+      {
+        "name": "some-uri-DIGEST",
+        "value": "sha256:hash123"
+      },
+      {
+        "name": "some-uri",
+        "value": "pkg:deb/debian/curl@7.50.3-1"
+      }
+    ],
+    "taskSpec": {
+      "params": [],
+      "results": [
+        {
+          "name": "some-uri-DIGEST",
+          "description": "Digest of a file to push."
+        },
+        {
+          "name": "some-uri",
+          "description": "some calculated uri"
+        }
+      ]
+    }
+  }
+}
+`
+
+var expected2 = in_toto.Provenance{
+	Attestation: in_toto.Attestation{
+		AttestationType: in_toto.ProvenanceTypeV1,
+		Subject: in_toto.ArtifactCollection{
+			"pkg:deb/debian/curl@7.50.3-1": in_toto.ArtifactDigest{
+				"sha256": "hash123",
+			},
+		},
+		Materials: in_toto.ArtifactCollection{
+			"gcr.io/test1/test1": in_toto.ArtifactDigest{
+				"sha256": "hash1",
+			},
+		},
+	},
+	Builder: in_toto.Builder{
+		ID: "test-pod-name",
+	},
+	Recipe: in_toto.Recipe{
+		Type:       tektonID,
+		EntryPoint: "test-task",
+	},
 }
 
 func TestInTotoIte6_CreatePayload1(t *testing.T) {
@@ -136,7 +242,24 @@ func TestInTotoIte6_CreatePayload1(t *testing.T) {
 	i := &InTotoIte6{}
 	got, err := i.CreatePayload(&tr)
 
-	if diff := cmp.Diff(got, expected); diff != "" {
+	if diff := cmp.Diff(expected1, got); diff != "" {
+		t.Errorf("InTotoIte6.CreatePayload(): -want +got: %s", diff)
+	}
+}
+
+func TestInTotoIte6_CreatePayload2(t *testing.T) {
+	var tr v1beta1.TaskRun
+
+	err := json.Unmarshal([]byte(testData2), &tr)
+	if err != nil {
+		t.Errorf("json.Unmarshal() error = %v", err)
+		return
+	}
+
+	i := &InTotoIte6{}
+	got, err := i.CreatePayload(&tr)
+
+	if diff := cmp.Diff(expected2, got); diff != "" {
 		t.Errorf("InTotoIte6.CreatePayload(): -want +got: %s", diff)
 	}
 }
