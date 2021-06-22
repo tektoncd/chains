dd144d85866ed31c1e557c58f8b4cbebb1efb53e
Priya Wadhwa <priyawadhwa@google.com>
2021-06-22 12:22:21 -0700

    Fix docdb behavior on retries
    Replaces `Create` with `Put`, which will create the document if it doesn't exist or replace it if it does.
    
    Now, on retries, this storage backend shouldn't fail if the document already exists. This also mimics what we do in other storage backends.

--- pkg/chains/storage/docdb/docdb.go
+++ pkg/chains/storage/docdb/docdb.go
@@ -77,7 +77,7 @@ func (b *Backend) StorePayload(signed []byte, signature string, key string) erro
 		Name:      key,
 	}
 
-	if err := b.coll.Create(context.Background(), &entry); err != nil {
+	if err := b.coll.Put(context.Background(), &entry); err != nil {
 		return err
 	}
 
