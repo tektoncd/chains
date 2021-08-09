e211f4bcfe7bbdb3fa7009f0b386dc92fe2a7ee2
Priya Wadhwa <priyawadhwa@google.com>
2021-08-09 11:54:29 -0400

    Increase timeout on golangci-lint to 10m

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -31,8 +31,8 @@ source "$(git rev-parse --show-toplevel)"/vendor/github.com/tektoncd/plumbing/sc
 
 function post_build_tests() {
   header "running golangci-lint"
-  # deadline of 5m, and show all the issues
-  golangci-lint --color=never run
+  # deadline of 10m, and show all the issues
+  golangci-lint --color=never --timeout 10m run
 }
 
 # We use the default build, unit and integration test runners.
