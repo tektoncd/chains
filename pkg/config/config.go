8b29855fca5574de429fb20234cf66aefe003119
Priya Wadhwa <priya@chainguard.dev>
2022-03-01 12:52:11 -0800

    Revert default fulcio URL to fulcio.sigstore.dev now, fulcio.sigstore.dev and v1.fulcio.sigstore.dev point to the same service, so we can switch back.

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -152,7 +152,7 @@ func defaultConfig() *Config {
 		},
 		Signers: SignerConfigs{
 			X509: X509Signer{
-				FulcioAddr: "https://v1.fulcio.sigstore.dev",
+				FulcioAddr: "https://fulcio.sigstore.dev",
 			},
 		},
 		Builder: BuilderConfig{
