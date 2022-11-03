b0c9c54930bce7d8288a3c68447fd4cac1b14176
Luiz Carvalho <lucarval@redhat.com>
2022-11-03 14:51:45 -0400

    Stop reconciliation on unrecoverable error (#607)
    Fixes: #602
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>
    
    Signed-off-by: Luiz Carvalho <lucarval@redhat.com>

--- pkg/reconciler/pipelinerun/pipelinerun_test.go
+++ pkg/reconciler/pipelinerun/pipelinerun_test.go
@@ -241,6 +241,35 @@ func TestReconciler_handlePipelineRun(t *testing.T) {
 			shouldSign: false,
 			wantErr:    true,
 		},
+		{
+			name: "missing taskrun",
+			pr: &v1beta1.PipelineRun{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:        "pipelinerun",
+					Namespace:   "default",
+					Annotations: map[string]string{},
+				},
+				Status: v1beta1.PipelineRunStatus{
+					Status: duckv1beta1.Status{
+						Conditions: []apis.Condition{{Type: apis.ConditionSucceeded}},
+					},
+					PipelineRunStatusFields: v1beta1.PipelineRunStatusFields{
+						TaskRuns: map[string]*v1beta1.PipelineRunTaskRunStatus{
+							"taskrun1": {
+								PipelineTaskName: "task1",
+								Status: &v1beta1.TaskRunStatus{
+									TaskRunStatusFields: v1beta1.TaskRunStatusFields{
+										CompletionTime: &metav1.Time{},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			shouldSign: false,
+			wantErr:    false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
