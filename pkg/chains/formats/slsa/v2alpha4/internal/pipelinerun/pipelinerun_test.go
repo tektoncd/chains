dea2e34e1f43a7652a4d10ffbc02ff65d4bddf47
Renzo Rojas Silva <renzor@google.com>
2024-06-04 13:39:17 -0400

    Fix no repeated subjects for v2alpha4 pipelineruns
    Change the pipelinerun v2alpha4 method that calculates the subjects to use `artifact.AppendSubjects` instead of `append` to avoid duplicated subjects.

--- pkg/chains/formats/slsa/v2alpha4/internal/pipelinerun/pipelinerun_test.go
+++ pkg/chains/formats/slsa/v2alpha4/internal/pipelinerun/pipelinerun_test.go
@@ -75,13 +75,13 @@ func TestByProducts(t *testing.T) {
 
 func TestGenerateAttestation(t *testing.T) {
 	ctx := logtesting.TestContextWithLogger(t)
-	pr := createPro("../../../testdata/slsa-v2alpha4/pipelinerun1.json")
 
 	e1BuildStart := time.Unix(1617011400, 0)
 	e1BuildFinished := time.Unix(1617011415, 0)
 
 	tests := []struct {
 		name                         string
+		pr                           *objects.PipelineRunObjectV1
 		expectedStatement            *intoto.Statement
 		expectedPredicate            *slsa.Provenance
 		expectedSubjects             []*intoto.ResourceDescriptor
@@ -91,6 +91,7 @@ func TestGenerateAttestation(t *testing.T) {
 	}{
 		{
 			name: "attestation without deepinspection",
+			pr:   createPro("../../../testdata/slsa-v2alpha4/pipelinerun1.json", "../../../testdata/slsa-v2alpha4/taskrun1.json", "../../../testdata/slsa-v2alpha4/taskrun2.json"),
 			expectedSubjects: []*intoto.ResourceDescriptor{
 				{
 					Name: "abc",
@@ -166,6 +167,7 @@ func TestGenerateAttestation(t *testing.T) {
 		},
 		{
 			name:               "attestation with deepinspection",
+			pr:                 createPro("../../../testdata/slsa-v2alpha4/pipelinerun1.json", "../../../testdata/slsa-v2alpha4/taskrun1.json", "../../../testdata/slsa-v2alpha4/taskrun2.json"),
 			withDeepInspection: true,
 			expectedSubjects: []*intoto.ResourceDescriptor{
 				{
@@ -287,11 +289,144 @@ func TestGenerateAttestation(t *testing.T) {
 				},
 			},
 		},
+		{
+			name:               "attestation with no repetead subjects",
+			pr:                 createPro("../../../testdata/slsa-v2alpha4/pipelinerun2.json", "../../../testdata/slsa-v2alpha4/taskrun3.json", "../../../testdata/slsa-v2alpha4/taskrun4.json"),
+			withDeepInspection: true,
+			expectedSubjects: []*intoto.ResourceDescriptor{
+				{
+					Name: "abc",
+					Digest: common.DigestSet{
+						"sha256": "827521c857fdcd4374f4da5442fbae2edb01e7fbae285c3ec15673d4c1daecb7",
+					},
+				},
+				{
+					Name: "gcr.io/common/image",
+					Digest: common.DigestSet{
+						"sha256": "33e7e52645f4859622e282167d9200da9861b3d0a6e9c93b85e9cae5526ffc0a",
+					},
+				},
+				{
+					Name: "test.io/test/image",
+					Digest: common.DigestSet{
+						"sha256": "827521c857fdcd4374f4da5442fbae2edb01e7fbae285c3ec15673d4c1daecb7",
+					},
+				},
+				{
+					Name: "gcr.io/task2/step/artifact",
+					Digest: common.DigestSet{
+						"sha256": "cb06e289303c9529cd980657a5b1a2c8a146c1b13ca08a2bbedb72ec4b7573b9",
+					},
+				},
+				{
+					Name: "gcr.io/my/image/fromstep3",
+					Digest: common.DigestSet{
+						"sha256": "827521c857fdcd4374f4da5442fbae2edb01e7fbae285c3ec15673d4c1daecb7",
+					},
+				},
+				{
+					Name: "gcr.io/my/image",
+					Digest: common.DigestSet{
+						"sha256": "d31cc8328054de2bd93735f9cbf0ccfb6e0ee8f4c4225da7d8f8cb3900eaf466",
+					},
+				},
+				{
+					Name: "gcr.io/task1/result",
+					Digest: common.DigestSet{
+						"sha256": "c6262181543796435ae52eb233d7337ec570ff0448e333460122f4a65a59a96a",
+					},
+				},
+			},
+			expectedResolvedDependencies: []*intoto.ResourceDescriptor{
+				{
+					Uri:    "git+https://github.com/test",
+					Digest: common.DigestSet{"sha1": "28b123"},
+					Name:   "pipeline",
+				},
+				{
+					Uri:    "git+https://github.com/catalog",
+					Digest: common.DigestSet{"sha1": "x123"},
+					Name:   "pipelineTask",
+				},
+				{
+					Uri:    "oci://gcr.io/test1/test1",
+					Digest: common.DigestSet{"sha256": "d4b63d3e24d6eef04a6dc0795cf8a73470688803d97c52cffa3c8d4efd3397b6"},
+				},
+				{
+					Uri:    "git+https://github.com/test",
+					Digest: common.DigestSet{"sha1": "ab123"},
+					Name:   "pipelineTask",
+				},
+				{
+					Uri:    "oci://gcr.io/test2/test2",
+					Digest: common.DigestSet{"sha256": "4d6dd704ef58cb214dd826519929e92a978a57cdee43693006139c0080fd6fac"},
+				},
+				{
+					Uri:    "oci://gcr.io/test3/test3",
+					Digest: common.DigestSet{"sha256": "f1a8b8549c179f41e27ff3db0fe1a1793e4b109da46586501a8343637b1d0478"},
+				},
+				{
+					Name:   "inputs/result",
+					Uri:    "https://github.com/tektoncd/pipeline",
+					Digest: common.DigestSet{"sha1": "7f2f46e1b97df36b2b82d1b1d87c81b8b3d21601"},
+				},
+				{
+					Uri:    "abc",
+					Digest: common.DigestSet{"sha256": "827521c857fdcd4374f4da5442fbae2edb01e7fbae285c3ec15673d4c1daecb7"},
+					Name:   "inputs/result",
+				},
+				{
+					Name:   "inputs/result",
+					Uri:    "git+https://git.test.com.git",
+					Digest: common.DigestSet{"sha1": "taskrun"},
+				},
+				{
+					Name:   "inputs/result",
+					Uri:    "git+https://git.test.com.git",
+					Digest: common.DigestSet{"sha1": "abcd"},
+				},
+			},
+			expectedByProducts: []*intoto.ResourceDescriptor{
+				{
+					Name:      "pipelineRunResults/CHAINS-GIT_COMMIT",
+					Content:   []uint8(`"abcd"`),
+					MediaType: JSONMediaType,
+				}, {
+					Name:      "pipelineRunResults/CHAINS-GIT_URL",
+					Content:   []uint8(`"https://git.test.com"`),
+					MediaType: JSONMediaType,
+				}, {
+					Name:      "pipelineRunResults/img-ARTIFACT_INPUTS",
+					Content:   []uint8(`{"digest":"sha256:827521c857fdcd4374f4da5442fbae2edb01e7fbae285c3ec15673d4c1daecb7","uri":"abc"}`),
+					MediaType: JSONMediaType,
+				}, {
+					Name:      "pipelineRunResults/img_no_uri-ARTIFACT_OUTPUTS",
+					Content:   []uint8(`{"digest":"sha256:827521c857fdcd4374f4da5442fbae2edb01e7fbae285c3ec15673d4c1daecb7"}`),
+					MediaType: JSONMediaType,
+				}, {
+					Name:      "taskRunResults/some-uri_DIGEST",
+					Content:   []uint8(`"sha256:d4b63d3e24d6eef04a6dc0795cf8a73470688803d97c52cffa3c8d4efd3397b6"`),
+					MediaType: JSONMediaType,
+				}, {
+					Name:      "taskRunResults/some-uri",
+					Content:   []uint8(`"pkg:deb/debian/curl@7.50.3-1"`),
+					MediaType: JSONMediaType,
+				}, {
+					Name:      "stepResults/step1_result1-ARTIFACT_INPUTS",
+					Content:   []uint8(`{"digest":"sha1:7f2f46e1b97df36b2b82d1b1d87c81b8b3d21601","uri":"https://github.com/tektoncd/pipeline"}`),
+					MediaType: JSONMediaType,
+				}, {
+					Name:      "stepResults/step1_result1",
+					Content:   []uint8(`"result-value"`),
+					MediaType: JSONMediaType,
+				},
+			},
+		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			got, err := GenerateAttestation(ctx, pr, &slsaconfig.SlsaConfig{
+			got, err := GenerateAttestation(ctx, test.pr, &slsaconfig.SlsaConfig{
 				BuilderID:             "test_builder-1",
 				DeepInspectionEnabled: test.withDeepInspection,
 				BuildType:             "https://tekton.dev/chains/v2/slsa",
@@ -305,7 +440,7 @@ func TestGenerateAttestation(t *testing.T) {
 				BuildDefinition: &slsa.BuildDefinition{
 					BuildType: "https://tekton.dev/chains/v2/slsa",
 					ExternalParameters: getStruct(t, map[string]any{
-						"runSpec": pr.Spec,
+						"runSpec": test.pr.Spec,
 					}),
 					InternalParameters:   getStruct(t, map[string]any{}),
 					ResolvedDependencies: test.expectedResolvedDependencies,
@@ -339,22 +474,21 @@ func TestGenerateAttestation(t *testing.T) {
 	}
 }
 
-func createPro(path string) *objects.PipelineRunObjectV1 {
-	pr, err := objectloader.PipelineRunFromFile(path)
-	if err != nil {
-		panic(err)
-	}
-	tr1, err := objectloader.TaskRunFromFile("../../../testdata/slsa-v2alpha4/taskrun1.json")
-	if err != nil {
-		panic(err)
-	}
-	tr2, err := objectloader.TaskRunFromFile("../../../testdata/slsa-v2alpha4/taskrun2.json")
+func createPro(prPath string, trPaths ...string) *objects.PipelineRunObjectV1 {
+	pr, err := objectloader.PipelineRunFromFile(prPath)
 	if err != nil {
 		panic(err)
 	}
 	p := objects.NewPipelineRunObjectV1(pr)
-	p.AppendTaskRun(tr1)
-	p.AppendTaskRun(tr2)
+
+	for _, trPath := range trPaths {
+		tr, err := objectloader.TaskRunFromFile(trPath)
+		if err != nil {
+			panic(err)
+		}
+		p.AppendTaskRun(tr)
+	}
+
 	return p
 }
 
