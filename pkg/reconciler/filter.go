85586f0c042f69be87ffed7f12720f79dd7f044e
jinjingroad <jinjingroad@sina.com>
2025-06-20 21:57:45 +0800

    refactor: use slices.Contains to simplify code (#1380)
    Signed-off-by: jinjingroad <jinjingroad@sina.com>

--- pkg/reconciler/filter.go
+++ pkg/reconciler/filter.go
@@ -16,6 +16,7 @@ package reconciler
 import (
 	v1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	"knative.dev/pkg/controller"
+	"slices"
 )
 
 // PipelineRunInformerFilterFunc returns a filter function
@@ -27,10 +28,8 @@ func PipelineRunInformerFilterFunc(namespaces []string) func(obj interface{}) bo
 			return true
 		}
 		if pr, ok := obj.(*v1.PipelineRun); ok {
-			for _, ns := range namespaces {
-				if pr.Namespace == ns {
-					return true
-				}
+			if slices.Contains(namespaces, pr.Namespace) {
+				return true
 			}
 		}
 		return false
@@ -46,10 +45,8 @@ func TaskRunInformerFilterFunc(namespaces []string) func(obj interface{}) bool {
 			return true
 		}
 		if tr, ok := obj.(*v1.TaskRun); ok {
-			for _, ns := range namespaces {
-				if tr.Namespace == ns {
-					return true
-				}
+			if slices.Contains(namespaces, tr.Namespace) {
+				return true
 			}
 		}
 		return false
@@ -69,10 +66,8 @@ func TaskRunInformerFilterFuncWithOwnership(namespaces []string) func(obj interf
 			return true
 		}
 		if tr, ok := obj.(*v1.TaskRun); ok {
-			for _, ns := range namespaces {
-				if tr.Namespace == ns {
-					return true
-				}
+			if slices.Contains(namespaces, tr.Namespace) {
+				return true
 			}
 		}
 		return false
